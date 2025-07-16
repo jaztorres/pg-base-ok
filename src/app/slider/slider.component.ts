1ae967cccc7bf92ee5933c04b7a7b3005a7541a8
jaztorres <jazmin.torres@myma.mx>
2025-07-15 22:49:40 -0600

    update

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -13,12 +13,15 @@ export class SliderComponent implements OnInit {
     { src: 'https://picsum.photos/id/1/800/400', alt: 'Slide 1' },
     { src: 'https://picsum.photos/id/2/800/400', alt: 'Slide 2' },
     { src: 'https://picsum.photos/id/3/800/400', alt: 'Slide 3' },
+    { src: 'assets/img/ima1.jpg', alt: 'Slide 4' },
+    { src: 'assets/img/ima2.jpg', alt: 'Slide 5' },
+    { src: 'assets/img/ima3.jpg', alt: 'Slide 6' },
   ];
   currentSlide = 0;
   transformStyle = 'translateX(0px)'; // Esto se actualizará dinámicamente
 
   // Define el ancho de la diapositiva para usarlo en el cálculo
-  slideWidth = 800; // Asegúrate de que esto coincida con el ancho de tus imágenes o contenedor
+  slideWidth = 1890; // Asegúrate de que esto coincida con el ancho de tus imágenes o contenedor
 
   ngOnInit(): void {
     this.updateSlider();
